7adb398e8ef9ef215b2b2ea34dc3392f0ebf0652
Romainducos <ducosromain31@gmail.com>
2025-09-08 14:36:00 +0200

    feat : changing to 1 button

--- js/index.js
+++ js/index.js
@@ -3,15 +3,23 @@ var timer = new easytimer.Timer();
 
 $('#countdownExample .values').html('02:00');
 
+var timerRunning = false;
+
 $('.startButton').click(function () {
-  timer.start({countdown: true, startValues: {minutes : 2}});
+  if (!timerRunning) {
+    timer.start({countdown: true, startValues: {minutes : 2}});
+    timerRunning = true;
+    $(this).text('Reset');
+  } else {
+    timer.reset();
+    $('#countdownExample .values').html('02:00');
+    timer.pause();
+    timerRunning = false;
+    $(this).text('Start');
+  }
 });
 
-$('.resetButton').click(function () {
-  timer.reset();
-  $('#countdownExample .values').html('02:00');
-  timer.pause();
-});
+
 
 timer.addEventListener('secondsUpdated', function (e) {
     $('#countdownExample .values').html(timer.getTimeValues().toString(['minutes', 'seconds']));
@@ -19,4 +27,6 @@ timer.addEventListener('secondsUpdated', function (e) {
 
 timer.addEventListener('targetAchieved', function (e) {
     $('#countdownExample .values').html('GG');
+    timerRunning = false;
+    $('.startButton').text('Start');
 });
