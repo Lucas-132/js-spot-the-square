766ce4144558cd4c3b5b7fa40b525569d7703a83
Lucas-132 <lucas.pollet135@gmail.com>
2025-09-10 14:13:57 +0200

    feat: added highscore functions
    Ready to use inside main.js (index.js)

---
+++ js/highscore.js
@@ -0,0 +1,21 @@
+export const highscoreDisplayer = document.getElementById("best-score-number");
+export let currentScore = 0;
+
+export function initializeHighscore(){
+    if (!sessionStorage.getItem('highscore1Min')){
+        sessionStorage.setItem('highscore1Min','0');
+    }
+    if (!sessionStorage.getItem('highscore2Min')){
+        sessionStorage.setItem('highscore2Min','0');
+    }
+    if (!sessionStorage.getItem('highscore1Survival')){
+        sessionStorage.setItem('highscore1Survival','0');
+    }
+}
+
+export function updateHighscore(selectedHighscore, currentScore){
+    if (currentScore > selectedHighscore){
+        selectedHighscore = currentScore;
+        highscoreDisplayer.innerHTML = selectedHighscore
+    }
+}
