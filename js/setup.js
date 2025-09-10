eb189fad49bc9f246050ae216ea40cff712d5c5b
LïamA <augu.liam@gmail.com>
2025-09-10 15:26:59 +0200

    feat : changed for chrono modes -> no more duration="" but mode=""

--- js/setup.js
+++ js/setup.js
@@ -6,25 +6,14 @@ et stocke les modes de jeu (chrono1/2 ou survie).
 
 import { currentBox, randomizeBox, randomizedBox } from "./boxRandomizer.js";
 
-function getDurationFromUrl() {
-  const params = new URLSearchParams(window.location.search);
-  const duration = parseInt(params.get("duration"), 10);
-  if (!isNaN(duration) && duration > 0) {
-    return duration;
-  }
-  return 2; // valeur par défaut
-}
-
 const params = new URLSearchParams(window.location.search);
 const mode = params.get("mode");
-const duration = getDurationFromUrl();
 const score = document.getElementById("score-number");
 const allTheBox = document.getElementsByClassName("case");
 let timer = new easytimer.Timer();
 
 export {
   mode,
-  duration,
   score,
   allTheBox,
   timer,
